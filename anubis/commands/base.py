7ab8c7f71b6982dbc517241e67b929bd5e7a166c
RikDan-CyberSecurity-Research <rikdan.randd@gmail.com>
2024-12-16 09:05:11 -0700

    Update base.py

--- anubis/commands/base.py
+++ anubis/commands/base.py
@@ -1,14 +1,32 @@
 """The base command."""
 
+from typing import Any, Dict
 
-class Base(object):
-  """A base command."""
 
-  def __init__(self, options, *args, **kwargs):
-    self.options = options
-    self.args = args
-    self.kwargs = kwargs
+class Base:
+    """A base command template.
 
-  def run(self):
-    raise NotImplementedError(
-      'run() method must be implemented by the overloading class')
+    This class is intended to be subclassed. Subclasses must implement the `run()` method.
+    """
+
+    def __init__(self, options: Dict[str, Any], *args: Any, **kwargs: Any) -> None:
+        """
+        Initialize the base command.
+
+        Args:
+            options (dict): Command options (e.g., CLI flags).
+            *args: Positional arguments.
+            **kwargs: Keyword arguments.
+        """
+        self.options = options
+        self.args = args
+        self.kwargs = kwargs
+
+    def run(self) -> None:
+        """Execute the command.
+
+        This method must be implemented in subclasses.
+        """
+        raise NotImplementedError(
+            'The run() method must be implemented by the subclass.'
+        )
