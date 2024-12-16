050101a269c85cea76a6b6c6ab842e285b6c41a5
RikDan-CyberSecurity-Research <rikdan.randd@gmail.com>
2024-12-16 08:27:00 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,52 +1,67 @@
 """Packaging settings."""
 
-from codecs import open
 from os.path import abspath, dirname, join
-from subprocess import call
+from subprocess import run, CalledProcessError
 
 from anubis import __version__
 from setuptools import Command, find_packages, setup
 
 this_dir = abspath(dirname(__file__))
 with open(join(this_dir, 'README.md'), encoding='utf-8') as file:
-  long_description = file.read()
+    long_description = file.read()
 
 
 class RunTests(Command):
-  """Run all tests."""
-  description = 'run tests'
-  user_options = []
+    """Run all tests."""
+    description = 'run tests'
+    user_options = []
 
-  def initialize_options(self):
-    pass
+    def initialize_options(self):
+        pass
 
-  def finalize_options(self):
-    pass
+    def finalize_options(self):
+        pass
 
-  def run(self):
-    """Run all tests!"""
-    errno = call(['py.test', '--cov=anubis', '--cov-report=term-missing'])
-    raise SystemExit(errno)
+    def run(self):
+        """Run all tests!"""
+        try:
+            result = run(['pytest', '--cov=anubis', '--cov-report=term-missing'], check=True)
+            raise SystemExit(result.returncode)
+        except CalledProcessError as e:
+            raise SystemExit(e.returncode)
+
 
 with open(join(this_dir, 'requirements.txt'), encoding='utf-8') as file:
-  reqs = file.read().split('\n')
+    reqs = [line.strip() for line in file if line.strip() and not line.startswith('#')]
 
-setup(name='anubis-netsec', version=__version__,
-      description='Modern and efficient subdomain enumeration and information gathering',
-      long_description=long_description,
-      long_description_content_type="text/markdown",
-      url='https://github.com/jonluca/anubis', author='JonLuca DeCaro',
-      author_email='jonluca.decaro@gmail.com', license='MIT',
-      classifiers=['Intended Audience :: Developers',
-                   'Development Status :: 3 - Alpha', 'Topic :: Utilities',
-                   'License :: Public Domain', 'Natural Language :: English',
-                   'Operating System :: OS Independent',
-                   'Programming Language :: Python :: 3.4',
-                   'Programming Language :: Python :: 3.5',
-                   'Programming Language :: Python :: 3.6', ], keywords='cli',
-      packages=find_packages(exclude=['docs', 'tests*']), python_requires='>=3',
-      install_requires=[reqs],
-      extras_require={'test': ['coverage', 'pytest', 'pytest-cov'], },
-      entry_points={'console_scripts': ['anubis=anubis.cli:main', ], },
-      cmdclass={'test': RunTests},
-      py_modules=["anubis.utils", "anubis.scanners"], )
+setup(
+    name='anubis-netsec-refactored',
+    version=__version__,
+    description='Modern and efficient subdomain enumeration and information gathering tool, overhall 2024 by rikdan cybersecurity research',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/RikDan-CyberSecurity-Research/Anubis',
+    author='JonLuca DeCaro',
+    author_email='jonluca.decaro@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Development Status :: 3 - Alpha',
+        'Topic :: Utilities',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+    keywords='cli',
+    packages=find_packages(exclude=['docs', 'tests*']),
+    python_requires='>=3.6',
+    install_requires=reqs,
+    extras_require={'test': ['coverage', 'pytest', 'pytest-cov']},
+    entry_points={'console_scripts': ['anubis=anubis.cli:main']},
+    cmdclass={'test': RunTests},
+)
